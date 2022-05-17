940846c6d384b8fd1488e49ac53cdff7aa18d4dc
Andrew Helsby <ajhelsby@hotmail.com>
2022-05-17 14:37:46 +0400

    python-sdk: Update flagsmith implementation

--- src/provider/flagsmith/flagsmith_provider.py
+++ src/provider/flagsmith/flagsmith_provider.py
@@ -1,4 +1,5 @@
 from numbers import Number
+from os import environ
 
 import flagsmith as Flagsmith
 
@@ -8,24 +9,27 @@ from src.provider.provider import AbstractProvider
 class FlagsmithProvider(AbstractProvider):
     def __init__(self, flagsmith: Flagsmith = None):
         if flagsmith is None:
-            self.flagsmith_provider = Flagsmith(environment_id="<YOUR_ENVIRONMENT_KEY>")
+            self.flagsmith_provider = Flagsmith(environment_id=environ.get("FLAGSMITH_ENVIRONMENT_KEY", None))
         else:
             self.flagsmith_provider = flagsmith
 
     def get_boolean_value(self, key: str, defaultValue: bool) -> bool:
         value = self.flagsmith_provider.get_value(key)
+        return value
 
     def get_boolean_details(self, key: str, defaultValue: bool):
         pass
 
     def get_string_value(self, key: str, defaultValue: bool) -> str:
         value = self.flagsmith_provider.get_value(key)
+        return value
 
     def get_string_details(self, key: str, defaultValue: bool):
         pass
 
     def get_number_value(self, key: str, defaultValue: bool) -> Number:
         value = self.flagsmith_provider.get_value(key)
+        return value
 
     def get_number_details(self, key: str, defaultValue: bool):
         pass
