c1db67ec5e31ff4baea82ffff9a10b415239499c
Andrew Helsby <ajhelsby@hotmail.com>
2022-07-01 12:00:26 +0400

    evaluation-context: Add evaluation context to open feature
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>

---
+++ open_feature/evaluation_context/evaluation_context.py
@@ -0,0 +1,17 @@
+class EvaluationContext:
+    def __init__(self, targeting_key: str = None, attributes: dict = None):
+        self.targeting_key = targeting_key
+        self.attributes = attributes
+
+    def merge(
+        self, ctx1: "EvaluationContext", ctx2: "EvaluationContext"
+    ) -> "EvaluationContext":
+        self.attributes = ctx1.attributes.update(ctx2.attributes)
+
+        if ctx1.targeting_key is not None:
+            self.targeting_key = ctx1.targeting_key
+
+        if ctx2.targeting_key is not None:
+            self.targeting_key = ctx2.targeting_key
+
+        return self
--- open_feature/open_feature_client.py
+++ open_feature/open_feature_client.py
@@ -1,8 +1,8 @@
 import typing
 from numbers import Number
 
+from open_feature.evaluation_context.evaluation_context import EvaluationContext
 from open_feature.exception.exceptions import GeneralError, OpenFeatureError
-from open_feature.flag_evaluation.error_code import ErrorCode
 from open_feature.flag_evaluation.flag_evaluation_details import FlagEvaluationDetails
 from open_feature.flag_evaluation.flag_type import FlagType
 from open_feature.flag_evaluation.reason import Reason
@@ -28,7 +28,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: bool,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> bool:
         return self.evaluate_flag_details(
@@ -43,7 +43,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: bool,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> FlagEvaluationDetails:
         return self.evaluate_flag_details(
@@ -58,7 +58,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: str,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> str:
         return self.evaluate_flag_details(
@@ -73,7 +73,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: str,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> FlagEvaluationDetails:
         return self.evaluate_flag_details(
@@ -88,7 +88,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: Number,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> Number:
         return self.evaluate_flag_details(
@@ -103,7 +103,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: Number,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> FlagEvaluationDetails:
         return self.evaluate_flag_details(
@@ -118,7 +118,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: dict,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> dict:
         return self.evaluate_flag_details(
@@ -133,7 +133,7 @@ class OpenFeatureClient:
         self,
         key: str,
         default_value: dict,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> FlagEvaluationDetails:
         return self.evaluate_flag_details(
@@ -149,34 +149,53 @@ class OpenFeatureClient:
         flag_type: FlagType,
         key: str,
         default_value: typing.Any,
-        evaluation_context: typing.Any = None,
+        evaluation_context: EvaluationContext = None,
         flag_evaluation_options: typing.Any = None,
     ) -> FlagEvaluationDetails:
+        if evaluation_context is None:
+            evaluation_context = EvaluationContext()
         try:
-            args = (
+            # The merge below will pull in Evaluation contexts from Hooks
+            invocation_context = EvaluationContext.merge(None, evaluation_context)
+            return self.create_provider_evaluation(
+                flag_type,
                 key,
                 default_value,
-                evaluation_context,
+                invocation_context,
                 flag_evaluation_options,
             )
-            if flag_type is FlagType.BOOLEAN:
-                return self.provider.get_boolean_details(*args)
-
-            elif flag_type is FlagType.NUMBER:
-                return self.provider.get_number_details(*args)
-
-            elif flag_type is FlagType.OBJECT:
-                return self.provider.get_object_details(*args)
-            # Fallback case is a string object
-            elif flag_type is FlagType.STRING:
-                return self.provider.get_string_details(*args)
-            else:
-                raise GeneralError(error_message="Unknown flag type")
-
-        except OpenFeatureError:
+        except OpenFeatureError as ofe:
             return FlagEvaluationDetails(
                 key=key,
                 value=default_value,
                 reason=Reason.ERROR,
-                error_code=ErrorCode.GENERAL,
+                error_code=ofe.error_message,
             )
+
+    def create_provider_evaluation(
+        self,
+        flag_type: FlagType,
+        key: str,
+        default_value: typing.Any,
+        evaluation_context: EvaluationContext = None,
+        flag_evaluation_options: typing.Any = None,
+    ):
+        args = (
+            key,
+            default_value,
+            evaluation_context,
+            flag_evaluation_options,
+        )
+        if flag_type is FlagType.BOOLEAN:
+            return self.provider.get_boolean_details(*args)
+
+        elif flag_type is FlagType.NUMBER:
+            return self.provider.get_number_details(*args)
+
+        elif flag_type is FlagType.OBJECT:
+            return self.provider.get_object_details(*args)
+        # Fallback case is a string object
+        elif flag_type is FlagType.STRING:
+            return self.provider.get_string_details(*args)
+        else:
+            raise GeneralError(error_message="Unknown flag type")
