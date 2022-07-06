67076182c9c3befa48f086c97629afbbd01f44a6
Andrew Helsby <ajhelsby@hotmail.com>
2022-07-06 11:42:45 +0400

    evaluation-context: Reduce code on context merge
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>

--- open_feature/evaluation_context/evaluation_context.py
+++ open_feature/evaluation_context/evaluation_context.py
@@ -7,11 +7,6 @@ class EvaluationContext:
         self, ctx1: "EvaluationContext", ctx2: "EvaluationContext"
     ) -> "EvaluationContext":
         self.attributes = ctx1.attributes.update(ctx2.attributes)
-
-        if ctx1.targeting_key is not None:
-            self.targeting_key = ctx1.targeting_key
-
-        if ctx2.targeting_key is not None:
-            self.targeting_key = ctx2.targeting_key
+        self.targeting_key = ctx2.targeting_key or ctx1.targeting_key
 
         return self
