9b27a6b6ffbc47cbe608e6a4d7a8817223f0ea52
agardnerit <adam@agardner.net>
2022-10-03 11:54:20 +1000

    update conftest.py
    Signed-off-by: agardnerit <adam@agardner.net>

--- tests/provider/conftest.py
+++ tests/provider/conftest.py
@@ -2,9 +2,15 @@ import pytest
 
 from open_feature import open_feature_api as api
 from open_feature.provider.no_op_provider import NoOpProvider
+from open_feature.provider.flagd_provider import FlagDProvider
 
 
 @pytest.fixture()
 def no_op_provider_client():
     api.set_provider(NoOpProvider())
+    return api.get_client()
+
+@pytest.fixture()
+def flagd_provider_client():
+    api.set_provider(FlagDProvider())
     return api.get_client()
