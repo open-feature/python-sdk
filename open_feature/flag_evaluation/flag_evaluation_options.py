fbf19eb8ad8806aae190f25268cdcd13909df78c
Andrew Helsby <ajhelsby@hotmail.com>
2022-11-02 16:52:10 +0400

    feature/spec-0.2.0: Overwrite init on FlagEvaluationOptions Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>

--- open_feature/flag_evaluation/flag_evaluation_options.py
+++ open_feature/flag_evaluation/flag_evaluation_options.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass, field
+from typing import List
 
 from open_feature.hooks.hook import Hook
 
 
 @dataclass
 class FlagEvaluationOptions:
-    hooks: list[Hook] = field(default_factory=list)
+    hooks: List[Hook] = field(default_factory=list)
     hook_hints: dict = field(default_factory=dict)
+
+    def __init__(self):
+        self.hooks = []
+        self.hook_hints = {}
