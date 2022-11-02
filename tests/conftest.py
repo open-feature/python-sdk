df0c03308346c5b3be7223edc162582c578b4678
Andrew Helsby <ajhelsby@hotmail.com>
2022-11-02 16:24:16 +0400

    fix: Unit tests (#28)
    * fix/unit-tests: Improve unit test coverage to ensure spec is followed
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Add test coverage around hooks
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Parametrize test_should_use_no_op_provider_if_none_provided
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Parametrize all flag type methods in open feature client
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Parametrize all flag type methods in open feature client
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Add fixture to clear global provider after each tests
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Mock hooks in tests
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Update docstring on clear_provider method
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: remove setup method in client tests
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    * fix/unit-tests: Remove invalid flag type check in private method _create_provider_evaluation and move to public method
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>
    
    Signed-off-by: Andrew Helsby <ajhelsby@hotmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,16 @@ from open_feature import open_feature_api as api
 from open_feature.provider.no_op_provider import NoOpProvider
 
 
+@pytest.fixture(autouse=True)
+def clear_provider():
+    """
+    For tests that use set_provider(), we need to clear the provider to avoid issues
+    in other tests.
+    """
+    yield
+    _provider = None  # noqa: F841
+
+
 @pytest.fixture()
 def no_op_provider_client():
     api.set_provider(NoOpProvider())
