4abe86c8256b48ec970ae5451f28bf8efeaac6ee
Tom Carrio <tom@carrio.dev>
2022-12-26 21:56:33 -0500

    feat: defaults to NoOpProvider
    ensures a provider will always be returned
    
    Signed-off-by: Tom Carrio <tom@carrio.dev>

--- open_feature/open_feature_api.py
+++ open_feature/open_feature_api.py
@@ -2,18 +2,15 @@ import typing
 
 from open_feature.exception.exceptions import GeneralError
 from open_feature.open_feature_client import OpenFeatureClient
+from open_feature.provider.no_op_provider import NoOpProvider
 from open_feature.provider.provider import AbstractProvider
 
-_provider: typing.Optional[AbstractProvider] = None
+_provider: AbstractProvider = NoOpProvider()
 
 
 def get_client(
     name: typing.Optional[str] = None, version: typing.Optional[str] = None
 ) -> OpenFeatureClient:
-    if _provider is None:
-        raise GeneralError(
-            error_message="Provider not set. Call set_provider before using get_client"
-        )
     return OpenFeatureClient(name=name, version=version, provider=_provider)
 
 
