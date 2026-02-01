e04c58bbdfb8d790149a194b66b093e2fb429155
Danju Visvanathan <danju.visvanathan@gmail.com>
2026-02-02 03:51:46 +1100

    add: add TrackingEventDetails definition
    Signed-off-by: Danju Visvanathan <danju.visvanathan@gmail.com>

--- openfeature/track/__init__.py
+++ openfeature/track/__init__.py
@@ -1,22 +1,18 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
 from collections.abc import Mapping, Sequence
 import typing
 
 TrackingAttribute = typing.TypeAlias = bool | int | float | str | Sequence["TrackingAttribute"] | Mapping[str, "TrackingAttribute"]
 
-@dataclass
 class TrackingEventDetails:
-    value: float | None = None
-    attributes: TrackingAttribute = field(default_factory=dict)
+    value: float | None
+    attributes: TrackingAttribute
+
+    def __init__(self, value: float | None = None, attributes: TrackingAttribute | None = None):
+        self.value = value
+        self.attributes = attributes or {}
 
     def add(self, key: str, value: TrackingAttribute) -> "TrackingEventDetails":
         self.attributes[key] = value
         return self
-    
-    def get_value(self) -> float:
-        return self.value
-    
-    def get_attributes(self) -> TrackingAttribute:
-        return self.attributes
